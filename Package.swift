c158dc80f8783bc60527382ffb8670cfebeb38aa
Manish Bhande <manish.bhande90@gmail.com>
2025-04-05 13:28:37 +0530

    updated ios 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RadioCheckboxButton",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
